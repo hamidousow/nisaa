0332f788c1cdacfc7a607d97baac95da3eadffd9
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-07-23 17:13:10 +0200

    update nav, update responsive

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,29 +8,33 @@ import nissaLogo from '@/assets/images/nisaa-logo.svg';
 
 const isMenuOpen = ref(false);
 
-const menuOpen = ref("menu-open");
+
+const menuActive = ref("nav-menu active");
 
 
 </script>
 
 <template>
-    <nav class="nav">
-        <div class="nav-content">
-            <a href="/" @clic="isMenuOpen = false">
+    <nav class="nav bg-clr-maroon">
+        <div class="nav-content container">
+            <RouterLink to="/" @click="isMenuOpen = false">
                 <img :src="nissaLogo" alt="nisaa logo" class="nav-brand">
-            </a>
-            <ul :class="[isMenuOpen ? 'menu-open' : 'nav-menu']">
+            </RouterLink>
+            <ul :class="[isMenuOpen ? menuActive : 'nav-menu']">
                 <li class="menu-link">
-                    <RouterLink to="/"> l'histoire de nisaa </RouterLink>
+                    <RouterLink to="/" @click="isMenuOpen = false" class="abril-fatface-font"> l'histoire de nisaa
+                    </RouterLink>
                 </li>
                 <li class="menu-link">
-                    <RouterLink to="/" @clic="isMenuOpen = false">les sagesses</RouterLink>
+                    <RouterLink to="/about" @click="isMenuOpen = false" class="abril-fatface-font">les sagesses
+                    </RouterLink>
                 </li>
                 <li class="menu-link">
-                    <RouterLink to="/" @clic="isMenuOpen = false">la box du mois</RouterLink>
+                    <RouterLink to="/" @click="isMenuOpen = false" class="abril-fatface-font">la box du mois
+                    </RouterLink>
                 </li>
                 <li class="menu-link">
-                    <RouterLink to="/" @clic="isMenuOpen = false">contact</RouterLink>
+                    <RouterLink to="/" @click="isMenuOpen = false" class="abril-fatface-font">contact</RouterLink>
                 </li>
             </ul>
             <div class="utils-wrapper">
@@ -57,7 +61,6 @@ const menuOpen = ref("menu-open");
     left: 0;
     right: 0;
     top: 0;
-    background-color: var(--clr-maroon);
     z-index: 99;
 }
 
@@ -66,7 +69,6 @@ const menuOpen = ref("menu-open");
     flex-direction: row;
     justify-content: space-between;
     width: 100%;
-
     padding: 20px;
     max-width: 1480px;
     position: relative;
@@ -80,18 +82,31 @@ const menuOpen = ref("menu-open");
 
 .nav-menu {
     display: none;
-    visibility: hidden;
-    transition: .5s;
+    height: 90vh;
+    flex-direction: column;
+    position: absolute;
+    align-items: center;
+    top: 65px;
+    right: 0;
+    left: 0;
+    padding: 25px;
+    background-color: var(--clr-maroon);
+    row-gap: 15px;
+}
 
+.nav-menu.active {
+    display: flex;
+    justify-content: space-evenly;
 }
 
 .menu-link {
     list-style: none;
-
 }
 
 .menu-link a {
     color: var(--clr-green);
+    text-decoration: none;
+    font-size: 31px;
 }
 
 .utils-wrapper {
@@ -106,18 +121,31 @@ const menuOpen = ref("menu-open");
     column-gap: 15px;
 }
 
-.menu-open {
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    align-items: center;
-    top: 65px;
-    right: 0;
-    left: 0;
-    padding: 25px;
-    background-color: var(--clr-maroon);
-    row-gap: 15px;
-    visibility: visible;
 
+
+@media screen and (min-width: 768px) {
+
+    .nav-menu {
+        display: flex;
+        position: initial;
+        flex-direction: row;
+        width: auto;
+        height: auto;
+        column-gap: 20px;
+        flex: 1;
+        padding: 0 15px;
+        background-color: transparent;
+        justify-content: center;
+    }
+
+    .menu-link a {
+        color: var(--clr-green);
+        text-decoration: none;
+        font-size: 21px;
+    }
+
+    .btn-toggle-menu {
+        display: none;
+    }
 }
 </style>
