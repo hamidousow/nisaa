39c30fa3f148bb8b5f37134f7a665f326dfcf797
Hamidou.Sow <hamidou.sow@hotmail.fr>
2024-08-02 16:05:02 +0200

    rename header to hero

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -4,27 +4,27 @@ import Button from "@/components/ButtonCta.vue";
 </script>
 
 <template>
-    <header class="bg-clr-maroon">
-        <div class="header-content container">
-            <div class="header-left">
-                <h1 class="header-title abril-fatface-font">Le soin du corps et de l'esprit</h1>
-                <p class="header-text futura-regular-font">Découvre la première box beauté et lifestyle inspirée des
+    <div class="bg-clr-maroon">
+        <div class="hero-content container">
+            <div class="hero-left">
+                <h1 class="hero-title abril-fatface-font">Le soin du corps et de l'esprit</h1>
+                <p class="hero-text futura-regular-font">Découvre la première box beauté et lifestyle inspirée des
                     principes et
                     valeurs de l'islam.</p>
                 <Button path="/">Découvrez nos produits</Button>
             </div>
-            <div class="header-container-image">
+            <div class="hero-container-image">
                 <img :src="heroImage" alt="femme">
             </div>
         </div>
 
-    </header>
+    </div>
 </template>
 
 
 
 <style>
-.header-content {
+.hero-content {
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
@@ -38,29 +38,29 @@ import Button from "@/components/ButtonCta.vue";
     overflow: hidden;
 }
 
-.header-left {
+.hero-left {
     display: flex;
     flex-direction: column;
     row-gap: 10px;
 }
 
-.header-title {
+.hero-title {
     font-size: clamp(27px, 2vw, 50px);
     font-weight: 500;
 }
 
-.header-text {
+.hero-text {
     font-size: clamp(16px, 2vw, 30px);
 }
 
-.header-container-image {
+.hero-container-image {
     width: 100%;
     height: 100%;
     /* max-height: 250px; */
     position: relative;
 }
 
-.header-container-image img {
+.hero-container-image img {
     position: absolute;
     bottom: 0;
     display: block;
@@ -75,7 +75,7 @@ import Button from "@/components/ButtonCta.vue";
 
 @media screen and (min-width: 567px) {
 
-    .header-content {
+    .hero-content {
         flex-direction: row;
         height: 550px;
         width: 100%;
@@ -83,19 +83,19 @@ import Button from "@/components/ButtonCta.vue";
         overflow: hidden;
     }
 
-    .header-left {
+    .hero-left {
         row-gap: 20px;
         z-index: 1;
         justify-content: center;
         flex: 2;
     }
 
-    .header-container-image {
+    .hero-container-image {
         max-height: inherit;
         flex: 1;
     }
 
-    .header-container-image img {
+    .hero-container-image img {
         right: -60%;
     }
 
@@ -106,28 +106,28 @@ import Button from "@/components/ButtonCta.vue";
 
 @media screen and (min-width: 768px) {
 
-    .header-container-image img {
+    .hero-container-image img {
         right: 0;
     }
 }
 
 @media screen and (min-width: 992px) {
-    .header-content {
+    .hero-content {
         height: 80vh;
     }
 
-    .header-left {
+    .hero-left {
         row-gap: 20px;
         z-index: 1;
         justify-content: center;
         flex: 1;
     }
 
-    .header-title {
+    .hero-title {
         font-size: 50px;
     }
 
-    .header-text {
+    .hero-text {
         font-size: 30px;
     }
 }
